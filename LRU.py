007298c0baa181ffa9e6851244638ff9362089c8
Rohith Kalluru <rohithkalluru@gmail.com>
2020-09-23 11:09:08 +0530

    working code for lru

--- LRU.py
+++ LRU.py
@@ -3,13 +3,32 @@ class lru:
     def __init__(self, size):
         self.size = size
         self.lru = []
-        self.lruCache = {}
+        self.lrucache = {}
 
-    def get(self, key, default=None):
-        pass
+    def get(self, key):
+        if key in self.lru:
+            return self.lrucache[key]
+        else:
+            return None
 
     def put(self, key):
-        pass
+        value = "www."+str(key)+".com"
+        if(len(self.lru) < self.size):
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)
+                self.lrucache[key] = value
+            else:
+                self.lru.append(key)
+                self.lrucache[key] = value
+        else:
+            val = self.lru.pop(0)
+            self.lru.append(key)
+            del self.lrucache[val]
+            self.lrucache[key] = value
 
     def get_cache(self):
-        pass
+        l = []
+        for k in reversed(self.lru):
+            l.append(k+" : "+self.lrucache[k])
+        return l
